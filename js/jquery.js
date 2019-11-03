ea10f5dc7ba3b37457e20a70d7cbb506580a44f5
sajid9505 <42285255+sajid9505@users.noreply.github.com>
2019-11-03 14:18:07 +0600

    counter for number of items in cart done, prouct viewing still incomplete

--- js/jquery.js
+++ js/jquery.js
@@ -9,6 +9,18 @@ $('.btn-continue').click(function(){
     window.open("index.html","_self")
 })
 
+//cart number
+cart_items = JSON.parse(window.localStorage.getItem('cart_items'))
+var cart_item_length
+if(cart_items != null){
+    cart_item_length = cart_items.length
+}
+else{
+    cart_item_length = 0
+}
+$('.card-bag').children("span").text(cart_item_length)
+
+
 // Inputting data into LocalStorage
 $('.btn-full').click(function(){
     console.log("submit button clicked")
@@ -23,7 +35,7 @@ $('.btn-full').click(function(){
     window.open('index.html',"_self")
 })
 
-
+//inputting default values
 var products = JSON.parse(window.localStorage.getItem('products'))
  console.log(products)
  if(products == null)
@@ -40,13 +52,29 @@ var products = JSON.parse(window.localStorage.getItem('products'))
      window.localStorage.setItem('products',JSON.stringify(products))
  }
 
+ //viewingproducts needs help pls
  $.each(products,function(i,val){
-    var insertproduct = $('<ul class="slidee"><li><div class="intro-item"><figure><img src="" alt="..."></figure><div class="product-info"><h5></h5><p></p><a href="#" class="site-btn btn-line">ADD TO CART</a></div></div><li></ul>')
+    var insertproduct = $('<ul class="slidee"><li><div class="intro-item"><figure><img src="" alt="..."><div class="product-info"><h5></h5><p></p><a href="#" class="site-btn btn-line cartnumber">ADD TO CART</a></div></div></figure><li></ul>')
     insertproduct.children("li").children("div").children("figure").children("img").attr('src',products[i].img)
-    insertproduct.children("li").children("div").children("div").children("h5").text(products[i].title)
-    console.log(products[i].title)
-    insertproduct.children("li").children("div").children("div").children("p").text(products[i].price + "$")
+    insertproduct.children("li").children("div").children("figure").children("div").children("h5").text(products[i].title)
+    insertproduct.children("li").children("div").children("figure").children("div").children("p").text(products[i].price + "$")
     $('.intro-slider').append(insertproduct)
 })
 
- 
+
+var cart_items = []
+$('.cartnumber').click(function(){
+   var cart_item = []
+   cart_item = $(this).parent().parent().children("h5").text()
+   
+   cart_items = JSON.parse(window.localStorage.getItem('cart_items'))
+   if(cart_items == null){
+       cart_items = [cart_item]
+   }
+   else{
+       cart_items.push(cart_item)
+   }
+   
+   window.localStorage.setItem('cart_items',JSON.stringify(cart_items))
+   location.reload(true)
+})
